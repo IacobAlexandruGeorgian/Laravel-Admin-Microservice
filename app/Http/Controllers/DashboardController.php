754080142f63a04d7ce8973c51690cd565d289e0
Iacob Alexandru <iacobalex11@yahoo.com>
2024-02-05 20:57:42 +0200

    chart bug query

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -14,8 +14,8 @@ class DashboardController extends Controller
         Gate::authorize('view', 'orders');
 
         $orders = Order::query()
-                    ->join('order_items', 'orders.id', '=', 'orders_items.order_id')
-                    ->selectRaw('DATE_FORMAT(orders.created_at, "%Y-%m-%d") as date', 'sum(order_items.quanity*order_items.price) as sum')
+                    ->join('order_items', 'orders.id', '=', 'order_items.order_id')
+                    ->selectRaw('DATE_FORMAT(orders.created_at, "%Y-%m-%d") as date, sum(order_items.quantity*order_items.price) as sum', [])
                     ->groupBy('date')
                     ->get();
 
